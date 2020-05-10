08beac944803dca7e7c44586feb942f44bf909f5
Zielonyy3 <zielak134@gmail.com>
2020-05-10 20:03:41 +0200

    Add results adding add showing results details

--- app.js
+++ app.js
@@ -159,106 +159,204 @@ class Exercise {
         this.name = name;
         this.image = image;
         this.description = description;
+        this.results = [];
+        this.exerciseCardContent = '';
+        this.exerciseCardHeader = '';
+        this.exerciseListDown = '';
+        this.addResultButton = '';
+        this.exerciseSecondRow = '';
     }
 
     showInDOM() {
+        const createCardHeader = () => {
+            const exerciseCardHeader = document.createElement('div');
+            exerciseCardHeader.classList.add('exerciseCardHeader');
+
+            const exerciseTitle = document.createElement('h2');
+            exerciseTitle.classList.add('exerciseTitle');
+            exerciseTitle.textContent = this.name;
+
+            exerciseCardHeader.appendChild(exerciseTitle);
+
+            for (let i = 0; i < 3; i++) {
+                const result = document.createElement('div');
+                result.classList.add('result', 'green');
+                result.textContent = '25kg';
+                exerciseCardHeader.appendChild(result);
+            }
+
+            const listDown = document.createElement('i');
+            listDown.classList.add('listDown', 'fas', 'fa-chevron-down');
+            this.exerciseListDown = listDown;
+            // this.exerciseListDown.addEventListener('click', this.showExerciseContent.bind(this));
+            exerciseCardHeader.appendChild(listDown);
+            return exerciseCardHeader;
+        }
+
+        const createCardContent = () => {
+            const exerciseFirstRow = document.createElement('div');
+            exerciseFirstRow.classList.add('exerciseFirstRow');
+            const img = document.createElement('img');
+            img.src = this.image;
+
+            const inputDiv = document.createElement('div');
+
+            const inputs = [{
+                name: 'weight',
+                placeholder: 'Ciężar'
+            }, {
+                name: 'reps',
+                placeholder: 'Liczba powtórzeń'
+            }, {
+                name: 'series',
+                placeholder: 'Liczba serii'
+            }]
+
+            for (let i = 0; i < 3; i++) {
+                const input = document.createElement('input');
+                input.type = 'number';
+                input.classList.add('exerciseInput');
+                input.name = inputs[i].name;
+                input.placeholder = inputs[i].placeholder;
+                inputDiv.appendChild(input);
+            }
+            const textArea = document.createElement('textarea');
+            textArea.name = 'exerciseDescription';
+            textArea.id = 'exerciseDescription';
+            textArea.placeholder = 'Opis';
+            textArea.rows = '9';
+
+            const button = document.createElement('button');
+            button.classList.add('addSeries');
+            button.textContent = 'Dodaj';
+            this.addResultButton = button;
+            this.addResultButton.addEventListener('click', this.addResult.bind(this))
+
+            const inputDiv2 = document.createElement('div');
+            inputDiv2.appendChild(textArea);
+            inputDiv2.appendChild(button);
+
+            exerciseFirstRow.appendChild(img);
+            exerciseFirstRow.appendChild(inputDiv);
+            exerciseFirstRow.appendChild(inputDiv2);
+
+            const exerciseSecondRow = document.createElement('div');
+            exerciseSecondRow.classList.add('exerciseSecondRow');
+            // for (let i = 0; i < 20; i++) {
+            //     const result = document.createElement('div');
+            //     result.classList.add('result', 'green');
+            //     result.textContent = '25kg';
+            //     exerciseSecondRow.appendChild(result);
+            // }
+
+            this.exerciseSecondRow = exerciseSecondRow;
+            const exerciseCardContent = document.createElement('div');
+            exerciseCardContent.classList.add('exerciseCardContent', 'hiddenContent');
+            exerciseCardContent.appendChild(exerciseFirstRow);
+            exerciseCardContent.appendChild(exerciseSecondRow);
+
+            return exerciseCardContent;
+        }
+
+        this.exerciseCardHeader = createCardHeader();
+        this.exerciseCardContent = createCardContent();
+        this.exerciseCardHeader.addEventListener('click', this.showExerciseContent.bind(this))
         const exerciseCard = document.createElement('div');
         exerciseCard.classList.add('exerciseCard');
-
-        const header = this.createCardHeader();
-        const content = this.createCardContent();
-        exerciseCard.appendChild(header);
-        exerciseCard.appendChild(content);
+        exerciseCard.appendChild(this.exerciseCardHeader);
+        exerciseCard.appendChild(this.exerciseCardContent);
 
         document.querySelector('.content').appendChild(exerciseCard);
 
     }
-
-    createCardHeader() {
-        const exerciseCardHeader = document.createElement('div');
-        exerciseCardHeader.classList.add('exerciseCardHeader');
-
-        const exerciseTitle = document.createElement('h2');
-        exerciseTitle.classList.add('exerciseTitle');
-        exerciseTitle.textContent = this.name;
-
-        exerciseCardHeader.appendChild(exerciseTitle);
-
-        for (let i = 0; i < 3; i++) {
-            const result = document.createElement('div');
-            result.classList.add('result', 'green');
-            result.textContent = '25kg';
-            exerciseCardHeader.appendChild(result);
+    showExerciseContent() {
+        if (this.exerciseCardContent.classList.contains('hiddenContent')) {
+            this.exerciseCardContent.style.transition = 'height .3s, min-height .3s, padding .01s';
+            this.exerciseCardContent.style.height = '350px';
+            setTimeout(() => this.exerciseCardContent.style.height = '', 300);
+            this.exerciseListDown.classList.remove('fa-chevron-down');
+            this.exerciseListDown.classList.add('fa-chevron-up');
+        } else {
+            this.exerciseCardContent.style.transition = 'height .3s, min-height .3s, padding .3s';
+            this.exerciseListDown.classList.remove('fa-chevron-up');
+            this.exerciseListDown.classList.add('fa-chevron-down');
         }
-
-        const listDown = document.createElement('i');
-        listDown.classList.add('listDown', 'fas', 'fa-chevron-down');
-        exerciseCardHeader.appendChild(listDown);
-        console.log(exerciseCardHeader);
-        return exerciseCardHeader;
+        this.exerciseCardContent.classList.toggle('hiddenContent');
+    }
+    colorizeResults() {
+        const resultTab = [...this.exerciseSecondRow.querySelectorAll('.result')];
+        const newTab = resultTab.map(result => result.textContent.slice(0, result.textContent.indexOf('kg')));
+        resultTab.forEach((result, i) => {
+            console.log(result);
+            if ((i > 0 && parseInt(result.textContent.slice(0, result.textContent.indexOf('kg'))) < parseInt(resultTab[i - 1].textContent.slice(0, resultTab[i - 1].textContent.indexOf('kg'))))) {
+                result.classList.remove('green');
+                result.classList.add('red');
+            }
+        });
     }
 
-    createCardContent() {
-        const exerciseFirstRow = document.createElement('div');
-        exerciseFirstRow.classList.add('exerciseFirstRow');
-        const img = document.createElement('img');
-        img.src = this.image;
-
-        const inputDiv = document.createElement('div');
-
-        const inputs = [{
-            name: 'weight',
-            placeholder: 'Ciężar'
-        }, {
-            name: 'reps',
-            placeholder: 'Liczba powtórzeń'
-        }, {
-            name: 'series',
-            placeholder: 'Liczba serii'
-        }]
-
-        for (let i = 0; i < 3; i++) {
-            const input = document.createElement('input');
-            input.type = 'number';
-            input.classList.add('exerciseInput');
-            input.name = inputs[i].name;
-            input.placeholder = inputs[i].placeholder;
-            inputDiv.appendChild(input);
-        }
-        const textArea = document.createElement('textarea');
-        textArea.name = 'exerciseDescription';
-        textArea.id = 'exerciseDescription';
-        textArea.placeholder = 'Opis';
-        textArea.rows = '9';
-
-        const button = document.createElement('button');
-        button.classList.add('addSeries');
-        button.textContent = 'Dodaj';
-
-        const inputDiv2 = document.createElement('div');
-        inputDiv2.appendChild(textArea);
-        inputDiv2.appendChild(button);
-
-        exerciseFirstRow.appendChild(img);
-        exerciseFirstRow.appendChild(inputDiv);
-        exerciseFirstRow.appendChild(inputDiv2);
-        console.log(exerciseFirstRow);
-
-        const exerciseSecondRow = document.createElement('div');
-        exerciseSecondRow.classList.add('exerciseSecondRow');
-        for (let i = 0; i < 20; i++) {
-            const result = document.createElement('div');
-            result.classList.add('result', 'green');
-            result.textContent = '25kg';
-            exerciseSecondRow.appendChild(result);
+    addResult() {
+        const propertiesTab = ["weight", "reps", "series", "exerciseDescription"];
+        const values = [];
+        let empty = 0;
+        propertiesTab.forEach((property, i) => {
+            if (property == 'exerciseDescription') {
+                values[i - empty] = this.exerciseCardContent.querySelector(`textarea[name="${property}"]`);
+            } else {
+                const input = this.exerciseCardContent.querySelector(`input[name="${property}"]`);
+                if (input.value)
+                    values[i - empty] = input;
+                else {
+                    empty++;
+                    input.classList.add('animateEmptyInput');
+                    setTimeout(() => input.classList.remove('animateEmptyInput'), 1000)
+                }
+            }
+        })
+        let newResult = 0;
+        if (values.length == propertiesTab.length) {
+            newResult = new Result(values[0].value, values[1].value, values[2].value, values[3].value);
+            values.forEach(input => input.value = '');
+            this.results.push(newResult);
+            this.exerciseSecondRow.appendChild(newResult.addResultToDOM());
+            this.colorizeResults();
         }
 
-        const exerciseCardContent = document.createElement('div');
-        exerciseCardContent.classList.add('exerciseCardContent');
-        exerciseCardContent.appendChild(exerciseFirstRow);
-        exerciseCardContent.appendChild(exerciseSecondRow);
+    }
 
-        return exerciseCardContent;
+}
+
+class Result {
+    constructor(weight, reps, series, description) {
+        this.date = Date.now();
+        this.weight = weight;
+        this.reps = reps;
+        this.series = series;
+        this.description = description;
+        this.resultWindow = document.querySelector('.resultWindow');
+    }
+    addResultToDOM() {
+        const div = document.createElement('div');
+        div.classList.add('result');
+        div.textContent = this.weight + 'kg';
+        div.addEventListener('mouseover', this.showResultWindow.bind(this));
+        div.addEventListener('mouseout', this.hideResultWindow.bind(this));
+        return div;
+    }
+
+    showResultWindow(e) {
+        if (e.target.classList.contains('red'))
+            this.resultWindow.querySelector('.resultWeight').classList.add('red');
+        else
+            this.resultWindow.querySelector('.resultWeight').classList.remove('red');
+        this.resultWindow.querySelector('.resultWeight').textContent = this.weight + 'kg';
+        this.resultWindow.querySelector('.repsDiv').textContent = this.reps;
+        this.resultWindow.querySelector('.seriesDiv').textContent = this.series;
+        this.resultWindow.querySelector('.resultDescription').textContent = this.description;
+        this.resultWindow.style.transform = 'translate(-50%, -50%) scale(1)';
+    }
+    hideResultWindow(e) {
+        this.resultWindow.style.transform = 'translate(-50%, -50%) scale(0)';
     }
 }
 
